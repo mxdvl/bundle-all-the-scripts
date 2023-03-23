84cb92584b8f2a946b68f20795a7bedea4e1ba3b
Max Duval <max.duval@theguardian.com>
2023-03-23 11:26:40 +0000

    refactor: no bundle no deno node only

---
+++ bundlers/none/src/fetch_articles.js
@@ -0,0 +1,12 @@
+fetch("https://content.guardianapis.com/search?q=bundle&api-key=test")
+  .then((r) => r.json())
+  .then(({ response }) => {
+    for (const article of response.results) {
+      const li = document.createElement("li");
+      const anchor = document.createElement("a");
+      anchor.setAttribute("href", article.webUrl);
+      anchor.innerText = article.webTitle;
+      li.appendChild(anchor);
+      document.querySelector("#articles")?.appendChild(li);
+    }
+  });
