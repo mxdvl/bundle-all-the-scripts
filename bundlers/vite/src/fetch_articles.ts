5921a8d7c63d385f2539167b97f53fc22a2ddf76
Max Duval <max.duval@theguardian.com>
2023-03-23 15:42:59 +0000

    feat: add build step

--- bundlers/vite/src/fetch_articles.ts
+++ bundlers/vite/src/fetch_articles.ts
@@ -1,18 +1,16 @@
-// Pros: we can use top-level await
 // Cons: we need to `export {}`
 
-const { response } = await fetch(
-  "https://content.guardianapis.com/search?q=bundle&api-key=test",
-)
-  .then((r) => r.json());
-
-for (const article of response.results) {
-  const li = document.createElement("li");
-  const anchor = document.createElement("a");
-  anchor.setAttribute("href", article.webUrl);
-  anchor.innerText = article.webTitle;
-  li.appendChild(anchor);
-  document.querySelector("#articles")?.appendChild(li);
-}
+fetch("https://content.guardianapis.com/search?q=bundle&api-key=test")
+  .then((r) => r.json())
+  .then(({ response }) => {
+    for (const article of response.results) {
+      const li = document.createElement("li");
+      const anchor = document.createElement("a");
+      anchor.setAttribute("href", article.webUrl);
+      anchor.innerText = article.webTitle;
+      li.appendChild(anchor);
+      document.querySelector("#articles")?.appendChild(li);
+    }
+  });
 
 export {};
