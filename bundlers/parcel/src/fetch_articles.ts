c3ec567b5a3d22188e2b50a280bb6fd4977c73f8
Max Duval <max.duval@theguardian.com>
2023-03-24 09:58:37 +0000

    feat: take only first 3 articles

--- bundlers/parcel/src/fetch_articles.ts
+++ bundlers/parcel/src/fetch_articles.ts
@@ -3,7 +3,7 @@
 fetch("https://content.guardianapis.com/search?q=bundle&api-key=test")
   .then((r) => r.json())
   .then(({ response }) => {
-    for (const article of response.results) {
+    for (const article of response.results.slice(0, 3)) {
       const li = document.createElement("li");
       const anchor = document.createElement("a");
       anchor.setAttribute("href", article.webUrl);
